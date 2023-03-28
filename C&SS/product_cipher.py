4dc17e43be76bc62362fe43582db1ab1f85407d2
Ashish Yadav <unixm98@gmail.com>
2023-03-28 21:53:37 +0530

    add css outputs doc file

--- C&SS/product_cipher.py
+++ C&SS/product_cipher.py
@@ -1,8 +1,11 @@
 import math
-def format_string(string):
-        return string.replace(" ","").upper()
 
-def cipherFunction(key,plain_text):
+
+def format_string(string):
+    return string.replace(" ", "").upper()
+
+
+def cipherFunction(key, plain_text):
     cipher_text = ""
     for char in plain_text:
         if char.isupper():
@@ -11,54 +14,56 @@ def cipherFunction(key,plain_text):
             cipher_text += chr((ord(char)+key-97) % 26+97)
     return cipher_text
 
-def encipher_rail_fence(text,depth):
+
+def encipher_rail_fence(text, depth):
     length = len(text)
     c = int(math.ceil(length/depth))
     matrix = [[0 for i in range(c)] for i in range(depth)]
     k = 0
     ans = ""
-    for i in range(0,c):
-        for j in range(0,depth):
-            if k< length:
+    for i in range(0, c):
+        for j in range(0, depth):
+            if k < length:
                 matrix[j][i] = text[k]
-                k=k+1
-               
+                k = k+1
+
             else:
                 matrix[j][i] = 'X'
-    
+
     for num in matrix:
-        # print("[",end="")
         for n in num:
-            # print(" "+n+" ",end="")
-            ans+=n
-        # print("]")
-    
+            ans += n
+
     return ans
 
-def decipher_rail_fence(text,depth):
+
+def decipher_rail_fence(text, depth):
     length = len(text)
     c = int(math.ceil(length/depth))
     matrix = [[0 for i in range(c)] for i in range(depth)]
     k = 0
     ans = ""
-    for i in range(0,depth):
-        for j in range(0,c):
-            if k< length:
+    for i in range(0, depth):
+        for j in range(0, c):
+            if k < length:
                 matrix[i][j] = text[k]
-                k=k+1
-               
+                k = k+1
+
             else:
-                matrix[i][j] = '_'
-    
-    for i in range(0,c):
-        for j in range(0,depth):
+                matrix[i][j] = 'X'
+
+    for i in range(0, c):
+        for j in range(0, depth):
             ans += matrix[j][i]
-    
+
     return ans
+
 
 def printt(string):
     for i in string:
-        print(i,end="")
+        print(i, end="")
+
+
 print("\n1.Encrypt 2.Decrypt")
 choice = int(input("Your choice: "))
 
@@ -68,20 +73,20 @@ if choice == 1:
     if choice == 1:
         plain_text = str(input("Plain text: "))
         key = int(input("Key: "))
-        temp = cipherFunction(key,plain_text)
+        temp = cipherFunction(key, plain_text)
         print("Caeser CT: {}".format(temp))
         depth = int(input("Depth: "))
-        print("Cipher Text: {}".format(encipher_rail_fence(temp,depth)))
-    
+        print("Cipher Text: {}".format(encipher_rail_fence(temp, depth)))
+
     elif choice == 2:
         plain_text = str(input("Plain text: "))
         depth = int(input("Depth: "))
         temp = format_string(plain_text)
-        ct = encipher_rail_fence(temp,depth)
+        ct = encipher_rail_fence(temp, depth)
         key = int(input("Key: "))
-        finalCt = cipherFunction(key,ct)
+        finalCt = cipherFunction(key, ct)
         print("Cipher Text: {}".format(finalCt))
-        
+
     else:
         print("Invalid input")
 
@@ -91,21 +96,20 @@ elif choice == 2:
     if choice == 1:
         plain_text = str(input("Cipher text: "))
         depth = int(input("Depth: "))
-        temp = decipher_rail_fence(plain_text,depth)
+        temp = decipher_rail_fence(plain_text, depth)
         key = int(input("Key: "))
-        finalPT = cipherFunction(-key,temp)
+        finalPT = cipherFunction(-key, temp)
         print("Plain Text: {}".format(finalPT))
-        
+
     elif choice == 2:
         plain_text = str(input("Cipher text: "))
         depth = int(input("Depth: "))
         key = int(input("Key: "))
-        temp = cipherFunction(-key,plain_text)
-        finalPT = decipher_rail_fence(temp,depth)
+        temp = cipherFunction(-key, plain_text)
+        finalPT = decipher_rail_fence(temp, depth)
         print("Plain Text: {}".format(finalPT))
     else:
         print("Invalid input")
-    
+
 else:
     print("Invalid input")
-        
